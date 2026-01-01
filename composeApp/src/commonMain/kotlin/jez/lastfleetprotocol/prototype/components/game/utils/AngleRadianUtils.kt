45c2d002e75e6791a7854a381ba2fc8daca7e80d
jez <5053926+Ninjars@users.noreply.github.com>
2026-01-01 19:57:31 +0000

    need to account for float rounding errors

--- composeApp/src/commonMain/kotlin/jez/lastfleetprotocol/prototype/components/game/utils/AngleRadianUtils.kt
+++ composeApp/src/commonMain/kotlin/jez/lastfleetprotocol/prototype/components/game/utils/AngleRadianUtils.kt
@@ -4,9 +4,8 @@ import com.pandulapeter.kubriko.helpers.extensions.rad
 import com.pandulapeter.kubriko.types.AngleRadians
 
 fun AngleRadians.rotateTowards(target: AngleRadians, maxChange: AngleRadians): AngleRadians {
-    val delta = target - this
-    val rotateNegative = delta.normalized.rad > delta
-
+    val delta = target.normalized.rad - this.normalized.rad
+    val rotateNegative = (delta.normalized - 0.0001f).rad > delta
     return if (rotateNegative) {
         if (-maxChange > delta) {
             this - maxChange
